2a2669b0df9cbbdad0245e142289351b8c86e1aa
Ben <Ben@LAPTOP-TDE9RIN8.local>
2020-03-26 16:39:59 -0700

    Update storage.ide-shm, storage.ide-wal, and 4 more files...

--- TheCoolCRPG/Player.cs
+++ TheCoolCRPG/Player.cs
@@ -48,6 +48,8 @@ namespace TheCoolCRPG
                     return;
                 }
             }
+
+            CurrentHitPoints = MaxHitPoints;
             CurrentLocation = loc;
         }
 
